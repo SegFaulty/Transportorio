2593456c8c963ad9a4f4c10e1439b91264b72617
Nick <nick.skelin@gmail.com>
2022-05-03 00:42:11 -0700

    Refactor: proper indentation and code style change

--- prototypes/map-gen-presets.lua
+++ prototypes/map-gen-presets.lua
@@ -1,42 +1,35 @@
-data:extend(
-{
-  {
-    type = "map-gen-presets",
-    name = "default",
-    -- default changes nothing
-    Singistics =
-    {
-      order = "a",
-	 
-basic_settings =
-      {
-        autoplace_controls =
-        {
-          ["enemy-base"] =
-          {
-            frequency = 0,
-            size = 0
-          },
-		  ["trees"] = { frequency = 0,  size = 0,  richness = 0 }
-        },
-	peaceful_mode = true,
-
-      },
-      advanced_settings =
-      {
-        enemy_evolution =
-        {
-         enabled = false
-        },
-        enemy_expansion =
-        {
-         enabled = false
-        },
-        pollution =
-        {
-         enabled = false
-        }
-      }
-    },
-  }
+data:extend({
+	{
+		type = "map-gen-presets",
+		name = "default",
+		-- default changes nothing
+		Singistics = {
+			order = "a",
+	 		basic_settings = {
+				autoplace_controls = {
+					["enemy-base"] = {
+						frequency = 0,
+						size = 0
+					},
+					["trees"] = {
+						frequency = 0,
+						size = 0,
+						richness = 0
+					}
+				},
+				peaceful_mode = true,
+			},
+			advanced_settings = {
+				enemy_evolution = {
+					enabled = false
+				},
+				enemy_expansion = {
+					enabled = false
+				},
+				pollution = {
+					enabled = false
+				}
+			}
+		},
+	}
 })
