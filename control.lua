e1485c67f98933ca4af233f1c60a2aa36de77b61
Nick <nick.skelin@gmail.com>
2022-05-09 00:37:03 -0700

    seperated search data into seperate file

--- control.lua
+++ control.lua
@@ -1,30 +1,4 @@
--- Search = require("data.Search")
-local Search = {
-	filter="any", -- any, product, ingredient
-	searched_item="",
-	item_type="unknown"
-}
-
--- create a new search table
-function Search:new(filter, searched_item, item_type)
-	item_type = item_type or self.item_type
-	search = {}
-	search = {filter=filter, searched_item=searched_item, item_type=item_type}
-	setmetatable(search, self)
-	self.__index = self
-
-	return search
-end
-
--- checks if a search object has the same contents as itself.
-function Search:compare(search)
-	for key, value in pairs(search) do
-		if self[key] ~= value then
-			return false
-		end
-	end
-	return true
-end
+Search = require("data.Search")
 
 DEBUG = true -- Used for debug, users should not enable
 local debugCount = 0 -- Stops debugging messages
