d6c935e918f63e260ba006a1dbd60179f46eb9b6
Nick <nick.skelin@gmail.com>
2022-05-11 02:55:47 -0700

    added hotkey to toggle menu

--- prototypes/keybindings.lua
+++ prototypes/keybindings.lua
@@ -1,9 +1,15 @@
 -- custom input / keybindings
 data:extend({
-  {
-    type = "custom-input",
-    name = "move_backwards_in_search_history",
-    key_sequence = "BACKSPACE",
-    action = "lua"
-  }
+	{
+		type = "custom-input",
+		name = "tro_move_backwards_in_search_history",
+		key_sequence = "BACKSPACE",
+		action = "lua"
+	},
+	{
+		type = "custom-input",
+		name = "tro_toggle_trade_menu",
+		key_sequence = "SHIFT + T",
+		action = "lua"
+	},
 })
